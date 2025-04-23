f3b3f11563ca3e875fcbd2e95c45af4889b8afe8
Ali Xurshetov <alixurshetov@gmail.com>
2025-04-23 18:58:58 +0500

    fix: Scrolling List under TabBarView

--- lib/features/home/presentation/parts/home.dart
+++ lib/features/home/presentation/parts/home.dart
@@ -6,60 +6,54 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(title: const Text("Home")),
-          body: ListView(
-            padding: EdgeInsets.all(16),
-            children: [
-              ListElement(bottom: 24,),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          ListElement(bottom: 24,),
 
-              TabBar(
-                labelColor: Colors.blue,
-                unselectedLabelColor: Colors.grey,
-                dividerColor: Colors.transparent,
-                indicatorSize: TabBarIndicatorSize.tab,
-                indicator: BoxDecoration(border: Border.all(color: Colors.blue, width: 2)),
-                tabs: [Tab(text: "Tab 1"), Tab(text: "Tab 2"), Tab(text: "Tab 3")],
-              ),
-              SizedBox(height: 24),
-              SizedBox(
-                height: 400,
-                child: TabBarView(
+          TabBar(
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.grey,
+            dividerColor: Colors.transparent,
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicator: BoxDecoration(border: Border.all(color: Colors.blue, width: 2)),
+            tabs: [Tab(text: "Tab 1"), Tab(text: "Tab 2"), Tab(text: "Tab 3")],
+          ),
+          SizedBox(height: 24),
+          SizedBox(
+            height: 400,
+            child: TabBarView(
+              children: [
+                Container(color: Colors.red.shade100, alignment: Alignment.center, child: Text("TabBarView 1")),
+                Column(
                   children: [
-                    Container(color: Colors.red.shade100, alignment: Alignment.center, child: Text("TabBarView 1")),
-                    Column(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            color: Colors.green.shade100,
-                            alignment: Alignment.center,
-                            child: Text("TabBarView 2"),
-                          ),
-                        ),
-                        SizedBox(height: 24),
-                        ListElement(bottom: 10,),
-                      ],
+                    Expanded(
+                      child: Container(
+                        color: Colors.green.shade100,
+                        alignment: Alignment.center,
+                        child: Text("TabBarView 2"),
+                      ),
                     ),
-                    Container(color: Colors.blue.shade100, alignment: Alignment.center, child: Text("TabBarView 3")),
+                    SizedBox(height: 24),
+                    ListElement(bottom: 10,),
                   ],
                 ),
-              ),
-              SizedBox(height: 24),
-              ListView.builder(
-                primary: false,
-                shrinkWrap: true,
-                itemCount: 20,
-                itemBuilder: (context, index) {
-                  return ListElement(bottom: 8,);
-                },
-              ),
-            ],
+                Container(color: Colors.blue.shade100, alignment: Alignment.center, child: Text("TabBarView 3")),
+              ],
+            ),
           ),
-        ),
+          SizedBox(height: 24),
+          ListView.builder(
+            primary: false,
+            shrinkWrap: true,
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return ListElement(bottom: 8,);
+            },
+          ),
+        ],
       ),
     );
   }
